987c9d78bb92fbe39ab1f04f26100fce9c8bd969
MohanPrasathSece <153342772+MohanPrasathSece@users.noreply.github.com>
2025-01-31 15:32:40 +0530

    Update inventoryRoutes.js

--- merninventory/backend/routes/inventoryRoutes.js
+++ merninventory/backend/routes/inventoryRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const InventoryItem = require('../models/InventoryItem');
 
-// Route to get all inventory items
 router.get('/', async (req, res) => {
   try {
     const items = await InventoryItem.find();
